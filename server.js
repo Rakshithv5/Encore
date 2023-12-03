4338253616d5112e4386a0df466b2dd16ee56b56
Rakshith V <rakshithv@microsoft.com>
2023-12-03 15:56:48 +0530

    App services change

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ passport.use(new OIDCStrategy(
 app.use(express.static(__dirname + "/public"));
 
 app.set('view engine', 'ejs');
+app.use(express.static('./client/build'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -306,4 +307,4 @@ app.get('/logout', function(req, res) {
 
 
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
